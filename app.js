9e93c01c4abd436f81f4eea8f3a53dbf4d364296
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2026-03-02 06:57:43 +0530

    Change server listen address to '0.0.0.0'
    Updated server listening configuration to bind to all interfaces.

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ app.get('/', (req, res) => {
 require('./socketHandler')(io);
 
 // ===== Start the server =====
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
