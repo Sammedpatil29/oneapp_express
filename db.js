14e4039f10c72524f5698dd1c8965106094f32ad
Sammed <sammed.patil29@gmail.com>
2026-03-05 18:18:09 +0530

    db string moved to env

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 // db.js
-
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Initialize Sequelize and connect to PostgreSQL
-const sequelize = new Sequelize('postgresql://neondb_owner:npg_Y6yzCORB1KSM@ep-late-frog-a1aglm0l-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,  // Set to true if you want to see the raw SQL queries
 });
